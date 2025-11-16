84e2a575c24c1741b370276380a463d6640de4a9
k1mjunyoung <me@kimjunyoung.com>
2025-11-16 12:01:19 +0900

    Initial commit

---
+++ src/main/java/judgeoverflow/JudgeoverflowApplication.java
@@ -0,0 +1,13 @@
+package judgeoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudgeoverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JudgeoverflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/judgeoverflow/JudgeoverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package judgeoverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudgeoverflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
